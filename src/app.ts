b3484c4c19c90b5fdb93bd634561a4ee7855ac36
Luis Lopez <luisangellopz.snchz999@gmail.com>
2022-03-07 12:24:40 -0600

    Actualizacion del proyecto

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
